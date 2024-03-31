fb7848d3c5d3255256240c7d8b126e5ebb8d8c23
vkiou <160496678+vkiou@users.noreply.github.com>
2024-04-01 01:25:11 +0300

    Add files via upload

---
+++ cooking_show-drop_objects.sql
@@ -0,0 +1,28 @@
+use cooking_show;
+
+#drop tables
+
+drop table cuisine_by_country;
+drop table etiquette;
+drop table tip;
+drop table equipment;
+drop table step;
+drop table recipe_time;
+drop table ingridient_group;
+drop table ingridient;
+drop table nutritional_info;
+drop table thematic_section;
+drop table recipe;
+drop table recipe_etiquette;
+drop table recipe_tip;
+drop table recipe_equipment;
+drop table recipe_step;
+drop table recipe_thematic_section;
+drop table recipe_nutritional_info;
+drop table cook;
+drop table cook_specialization;
+drop table winner;
+drop table season;
+drop table episode;
+drop table episode_cook_participant;
+drop table episode_cook_judge;
